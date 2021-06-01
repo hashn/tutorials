02675f4da78ed8803e5a356bb7ec4070e07d2364
HeshanNayanajith <heshannayanajith@ieee.org>
2021-06-01 22:50:01 +0530

    name changed

--- app/server.js
+++ app/server.js
@@ -22,7 +22,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome codl." });
 });
 
 // set port, listen for requests
